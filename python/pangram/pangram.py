a6c29243d389e1981f1b5cc0ca1890c3e065e0ac
Emile Bondzie-Arthur <frederickauthur@hotmail.com>
2020-12-17 02:59:40 +0000

    solved pangram question

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,8 @@
+import string
+
+
 def is_pangram(sentence):
-    pass
+    chars = [i.lower() for i in sentence if i.isalpha()]
+    _ascii_chars = string.ascii_lowercase
+
+    return all(char in chars for char in _ascii_chars)
