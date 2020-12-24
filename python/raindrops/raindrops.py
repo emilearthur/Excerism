efe65b383d4ccc4b2bbcd1a9c3d0248878497093
Emile Bondzie-Arthur <frederickauthur@hotmail.com>
2020-12-24 13:15:02 +0000

    keeping it simple

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -2,12 +2,10 @@ def convert(number):
     _factor = {3: 'Pling', 5: 'Plang', 7: 'Plong'}
     output = []
 
-    factors = [i for i in range(3, 9, 2) if number % i == 0]
+    for key, value in _factor.items():
+        if number % key == 0:
+            output.append(value)
 
-    if factors:
-        for num in factors:
-            for key, value in _factor.items():
-                if key == num:
-                    output.append(value)
+    if output:
         return ''.join(output)
     return str(number)
