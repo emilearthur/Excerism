02de76dd50d9abef4668d3f297eb9abbeafb1468
Emile Bondzie-Arthur <frederickauthur@hotmail.com>
2020-12-29 19:57:56 +0000

    done and test passed

---
+++ python/matrix/matrix.py
@@ -0,0 +1,12 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        self._row = [[int(i) for i in j.split(' ')] for j in
+                     matrix_string.split('\n')]
+
+        self._col = list(map(list, zip(*self._row)))
+
+    def row(self, index):
+        return self._row[index-1]
+
+    def column(self, index):
+        return self._col[index-1]
